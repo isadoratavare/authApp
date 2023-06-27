8c47864d938e61245b1cef95a6154f4f7fe0055f
isadoratavare <isadoratavare85@gmail.com>
2023-06-27 19:32:05 -0300

    Refactoring

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 function Container({children, height}: any) {
-  console.log(height);
   return (
     <View style={styles.main}>
+      <View style={styles.appName}>
+        <Text style={styles.appNameText}>AuthApp</Text>
+      </View>
       <View style={[styles.formContainer, {height}]}>{children}</View>
     </View>
   );
@@ -26,6 +28,15 @@ const styles = StyleSheet.create({
     padding: 20,
 
     alignItems: 'center',
+  },
+  appName: {
+    position: 'absolute',
+    top: 80,
+  },
+  appNameText: {
+    fontSize: 30,
+    fontWeight: '900',
+    color: 'white',
   },
 });
 
